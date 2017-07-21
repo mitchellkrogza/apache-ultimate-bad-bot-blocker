91c7580e528c6aae9995abe1c67a1692c5446b4d
Mitchell Krog <mitchellkrog@gmail.com>
2017-07-21 11:15:21 +0200

    Try upgrade apache to 2.4.18 or higher

--- .dev-tools/install-apache.sh
+++ .dev-tools/install-apache.sh
@@ -29,7 +29,12 @@
 
 # Install Apache2
 sudo apt-get update
-sudo apt-get install -y --force-yes apache2
+sudo apt-get install -y apache2 apache2-utils
+
+# Try Upgrade to latest Apache
+sudo add-apt-repository ppa:ondrej/apache2
+sudo apt-get update
+sudo apt-get upgrade
 
 # Copy our virtual host template to sites-enabled overwriting the default site conf
 sudo cp $TRAVIS_BUILD_DIR/.dev-tools/defaultsite.conf /etc/apache2/sites-available/000-default.conf
