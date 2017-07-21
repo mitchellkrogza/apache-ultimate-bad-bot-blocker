8254a083c1cbbf7f30b9b5df82264509978a3c24
Mitchell Krog <mitchellkrog@gmail.com>
2017-07-21 11:28:22 +0200

    Test 3 - Upgrading to newer Apache

--- .dev-tools/install-apache.sh
+++ .dev-tools/install-apache.sh
@@ -28,14 +28,16 @@
 # SOFTWARE.
 
 # Install Apache2
-sudo apt-get update
-sudo apt-get install -y apache2 apache2-utils
+#sudo apt-get update
+#sudo apt-get install -y apache2 apache2-utils
 
 # Try Upgrade to latest Apache
 sudo add-apt-repository ppa:ondrej/apache2 -y
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
+sudo apt-get update
+sudo apt-get install -y apache2 apache2-utils
+
+#sudo apt-get upgrade -y
+#sudo apt-get dist-upgrade -y
 
 # Copy our virtual host template to sites-enabled overwriting the default site conf
 sudo cp $TRAVIS_BUILD_DIR/.dev-tools/defaultsite.conf /etc/apache2/sites-available/000-default.conf
