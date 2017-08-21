70c934a76d7333eaac4771f76ee0874b7b4dae36
Mitchell Krog <mitchellkrog@gmail.com>
2017-08-21 16:36:46 +0200

    Update Files

--- .dev-tools/prepare-user-agents-htaccess.sh
+++ .dev-tools/prepare-user-agents-htaccess.sh
@@ -4,23 +4,40 @@
 # Copyright: Mitchell Krog - https://github.com/mitchellkrogza
 # Repo Url: https://github.com/mitchellkrogza/The-Big-List-of-Hacked-Malware-Web-Sites
 
-#Specify Input and Output File
-# ****************************
+##############################################################################
+#        ___                 __                                              #
+#       / _ | ___  ___ _____/ /  ___                                         #
+#      / __ |/ _ \/ _ `/ __/ _ \/ -_)                                        #
+#     /_/ |_/ .__/\_,_/\__/_//_/\__/                                         #
+#        __/_/        __   ___       __     ___  __         __               #
+#       / _ )___ ____/ /  / _ )___  / /_   / _ )/ /__  ____/ /_____ ____     #
+#      / _  / _ `/ _  /  / _  / _ \/ __/  / _  / / _ \/ __/  '_/ -_) __/     #
+#     /____/\_,_/\_,_/  /____/\___/\__/  /____/_/\___/\__/_/\_\\__/_/        #
+#                                                                            #
+##############################################################################
+
+# ******************************
+# Specify Input and Output Files
+# ******************************
+
 _input=$TRAVIS_BUILD_DIR/_generator_lists/bad-user-agents.list
 _output=$TRAVIS_BUILD_DIR/.dev-tools/_htaccess_generator_files/bad-user-agents.list
 
+# *****************
 # Truncate our file
 # *****************
+
 sudo truncate -s 0 $_output
 
+# ********************************************
 # Use sed to strip the \ out of the input file
-# ***************************************************
+# ********************************************
+
 sed 's/\\ / /g' $_input > $_output
 
-# Use cut to strip the domains out of the url strings
-# ***************************************************
-#cut -d'\' -f3 $_input > $_output
 
+# *************************************
 # Sort our output file and remove dupes
 # *************************************
+
 sort -u $_output -o $_output
--- Apache_2.4/update-apacheblocker.sh
+++ Apache_2.4/update-apacheblocker.sh
@@ -3,6 +3,18 @@
 # Copyright - https://github.com/mitchellkrogza
 # Project Url: https://github.com/mitchellkrogza/apache-ultimate-bad-bot-blocker
 
+##############################################################################
+#        ___                 __                                              #
+#       / _ | ___  ___ _____/ /  ___                                         #
+#      / __ |/ _ \/ _ `/ __/ _ \/ -_)                                        #
+#     /_/ |_/ .__/\_,_/\__/_//_/\__/                                         #
+#        __/_/        __   ___       __     ___  __         __               #
+#       / _ )___ ____/ /  / _ )___  / /_   / _ )/ /__  ____/ /_____ ____     #
+#      / _  / _ `/ _  /  / _  / _ \/ __/  / _  / / _ \/ __/  '_/ -_) __/     #
+#     /____/\_,_/\_,_/  /____/\___/\__/  /____/_/\___/\__/_/\_\\__/_/        #
+#                                                                            #
+##############################################################################
+
 # MAKE SURE you have your whitelist-ips.conf, whitelist-domains.conf, blacklist-ips.conf and bad-referrer-words.conf files in /etc/custom.d
 # A major change to using include files was introduced in
 # https://github.com/mitchellkrogza/apache-ultimate-bad-bot-blocker/releases/tag/2017-04-19.Build-173
