e31869c185b58d6469a58559e7296cc4c1201803
Mitchell Krog <mitchellkrog@gmail.com>
2017-10-14 13:12:00 +0200

    Update generate-htaccess.php

--- .dev-tools/generate-htaccess.php
+++ .dev-tools/generate-htaccess.php
@@ -85,13 +85,13 @@ class Generator
             $line = str_replace(' ', '\ ', $line);
 
             // convert internationalized domain names
-            if (preg_match('/[А-Яа-яЁёɢ]/u', $line)) {
+            //if (preg_match('/[А-Яа-яЁёɢ]/u', $line)) {
 
-                $IDN = new IdnaConvert();
+                //$IDN = new IdnaConvert();
 
-                $line = $IDN->encode($line);
+                //$line = $IDN->encode($line);
 
-            }
+            //}
 
             if (empty($line)) {
                 continue;
