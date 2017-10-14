50e20abdaf87bd0afc2c815ba6254d9d28fcb710
Mitchell Krog <mitchellkrog@gmail.com>
2017-10-14 10:59:11 +0200

    TEST Regex Mod User-Agents .htaccess

--- .dev-tools/generate-htaccess.php
+++ .dev-tools/generate-htaccess.php
@@ -79,7 +79,7 @@ class Generator
         $lines2 = array();
         while (($line = fgets($handle)) !== false) {
             //$line = trim(preg_replace('/\s\s+/', ' ', $line));
-            $line = trim(preg_replace('/\s+/', '\ ', $line));
+            //$line = trim(preg_replace('/\s+/', '\ ', $line));
 
             // convert internationalized domain names
             if (preg_match('/[А-Яа-яЁёɢ]/u', $line)) {
