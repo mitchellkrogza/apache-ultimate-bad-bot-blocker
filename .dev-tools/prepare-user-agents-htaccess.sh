c3bc77e97eb569e2accac783978a4af568f993e7
Mitchell Krog <mitchellkrog@gmail.com>
2017-10-14 10:21:28 +0200

    FIX Regex Problems with User-Agents section of .htaccess generator

--- .dev-tools/prepare-user-agents-htaccess.sh
+++ .dev-tools/prepare-user-agents-htaccess.sh
@@ -27,7 +27,7 @@ _output=$TRAVIS_BUILD_DIR/.dev-tools/_htaccess_generator_files/bad-user-agents.l
 # Truncate our file
 # *****************
 
-sudo truncate -s 0 $_output
+#sudo truncate -s 0 $_output
 
 # ********************************************
 # Use sed to strip the \ out of the input file
