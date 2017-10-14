02451782af08555185596b634cbe5a5cfe9007d7
Mitchell Krog <mitchellkrog@gmail.com>
2017-10-14 11:45:41 +0200

    FIX stripping backslashes out of input file .htaccess

--- .dev-tools/prepare-user-agents-htaccess.sh
+++ .dev-tools/prepare-user-agents-htaccess.sh
@@ -35,7 +35,7 @@ sudo truncate -s 0 $_output
 # ********************************************
 
 #sed 's/\\ / /g' $_input > $_output
-sed 's/\\//g' $_input > $_output
+sed 's/\\//g' $_input >$_output
 #sudo cp $_input $_output
 
 
