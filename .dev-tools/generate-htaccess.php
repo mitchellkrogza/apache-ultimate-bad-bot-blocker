541c300da048a937b13c9dc59f96802cebf83365
Mitchell Krog <mitchellkrog@gmail.com>
2017-10-15 10:57:43 +0200

    TEST Regex Formatting for User Agents in .htaccess (1)

--- .dev-tools/generate-htaccess.php
+++ .dev-tools/generate-htaccess.php
@@ -78,11 +78,12 @@ class Generator
         }
         $lines2 = array();
         while (($line = fgets($handle)) !== false) {
-            $line = trim(preg_replace('/\s\s+/', ' ', $line));
+            //$line = trim(preg_replace('/\s\s+/', ' ', $line));
             //$line = trim(preg_replace('/\s+/', '\ ', $line));
             //$line = trim(preg_replace('/+/', ' ', $line));
             //
-            $line = str_replace(' ', '\ ', $line);
+            //$line = str_replace(' ', '\ ', $line);
+    		$line = trim(preg_replace('/\s\s+/', '\s', $line));
 
             // convert internationalized domain names
             //if (preg_match('/[А-Яа-яЁёɢ]/u', $line)) {
