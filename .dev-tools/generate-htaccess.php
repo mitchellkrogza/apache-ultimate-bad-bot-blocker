dbdb91cec47c19f1e7037c2f63c910b5e00f9e43
Mitchell Krog <mitchellkrog@gmail.com>
2017-10-15 11:36:47 +0200

    TEST Regex for .htaccess (3)

--- .dev-tools/generate-htaccess.php
+++ .dev-tools/generate-htaccess.php
@@ -83,9 +83,15 @@ class Generator
             //$line = trim(preg_replace('/+/', ' ', $line));
             //
             //$line = str_replace(' ', '\ ', $line);
+            
+            //Nissar
     		//$line = trim(preg_replace('/\s\s+/', '\s', $line));
-    		$line = trim(preg_replace('/\s/', '\\s', $line)); // We replace space with '\s'
-    		$line = str_replace('/', '\/',$line); // We replace '/' with '\/' 
+    		//$line = trim(preg_replace('/\s/', '\\s', $line)); // We replace space with '\s'
+    		//$line = str_replace('/', '\/',$line); // We replace '/' with '\/' 
+                $line = trim(preg_replace('/\s/', '\s', $line)); // We replace space with '\s'
+    			$line = str_replace('/', '\/',$line); // We replace '/' with '\/' 
+            
+            
             // convert internationalized domain names
             //if (preg_match('/[А-Яа-яЁёɢ]/u', $line)) {
 
