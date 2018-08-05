1892377665cc35c80539dceb3d8b6bd207638961
Mitchell Krog <mitchellkrog@gmail.com>
2018-08-05 15:20:29 +0200

    Test Installing Apache 2.4.34

---
+++ .dev-tools/install-apache-2.4.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+# Setup Apache for Testing the Apache Ultimate Bad Bot Blocker on apache 2.4.34
+# Created by: Mitchell Krog (mitchellkrog@gmail.com)
+# Copyright: Mitchell Krog - https://github.com/mitchellkrogza
+# Repo Url: https://github.com/mitchellkrogza/apache-ultimate-bad-bot-blocker
+
+##############################################################################
+#        ___                 __                                              #
+#       / _ | ___  ___ _____/ /  ___                                         #
+#      / __ |/ _ \/ _ `/ __/ _ \/ -_)                                        #
+#     /_/ |_/ .__/\_,_/\__/_//_/\__/                                         #
+#        __/_/        __   ___       __     ___  __         __               #
+#       / _ )___ ____/ /  / _ )___  / /_   / _ )/ /__  ____/ /_____ ____     #
+#      / _  / _ `/ _  /  / _  / _ \/ __/  / _  / / _ \/ __/  '_/ -_) __/     #
+#     /____/\_,_/\_,_/  /____/\___/\__/  /____/_/\___/\__/_/\_\\__/_/        #
+#                                                                            #
+##############################################################################
+
+# MIT License
+
+# Copyright (c) 2017 Mitchell Krog - mitchellkrog@gmail.com
+# https://github.com/mitchellkrogza
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# ******************
+# Stop Apache 2.2.25
+# ******************
+
+echo "Stopping Apache 2.2"
+sudo /usr/local/apache2/bin/apachectl stop
+
+# **********************
+# Clean Up Apache 2.2.25
+# **********************
+
+sudo rm -rf /usr/local/apache2/
+ls -la /usr/local
+sudo rm -rf /tmp/zlib-1.2.11/
+sudo rm -rf /tmp/httpd-2.2.25/
+ls -la /tmp
+
+# ***************************************************
+# Install latest Apache from Repo: ppa:ondrej/apache2
+# ***************************************************
+
+sudo add-apt-repository ppa:ondrej/apache2 -y
+sudo apt-get update
+sudo apt-get install -y apache2 apache2-utils
+
+# **************************
+# Show Loaded apache Modules
+# **************************
+
+printf '%s\n%s\n%s\n\n' "#################################" "Show Loaded Apache Modules" "#################################"
+sudo apache2ctl -M
+
+# **************************
+# Show Apache Version
+# **************************
+
+printf '%s\n%s\n%s\n\n' "#####################################" "Show Apache Version Information" "#####################################"
+sudo apache2ctl -V
+
+# Get copy of apache2.conf for 2.4
+#sudo cp /usr/local/apache2/conf/httpd.conf ${TRAVIS_BUILD_DIR}/.dev-tools/_conf_files_for_testing/apache2.4.34/httpd.conf
+sudo cp /etc/apache2/apache2.conf ${TRAVIS_BUILD_DIR}/.dev-tools/_conf_files_for_testing/apache2.4.34/apache2.conf
+
+# Get copy of httpd-vhosts.conf for 2.4
+#sudo cp /usr/local/apache2/conf/extra/httpd-vhosts.conf ${TRAVIS_BUILD_DIR}/.dev-tools/_conf_files_for_testing/apache2.4.34/httpd-vhosts.conf
+
+# Stat directories
+ls -la /etc/apache2/sites-available/
+
+
+
+# ************************************
+# Put new httpd-vhosts.conf into place
+# ************************************
+#echo "Copy httpd-vhosts.conf"
+#sudo cp ${TRAVIS_BUILD_DIR}/.dev-tools/_conf_files_for_testing/apache2.2.25/httpd-vhosts.conf /usr/local/apache2/conf/extra/httpd-vhosts.conf
+
+# *************************************
+# Get files from Repo Apache_2.4
+# *************************************
+
+#sudo mkdir /etc/apache2/custom.d
+sudo wget https://raw.githubusercontent.com/mitchellkrogza/apache-ultimate-bad-bot-blocker/master/Apache_2.4/custom.d/globalblacklist.conf -O /etc/apache2/custom.d/globalblacklist.conf
+sudo wget https://raw.githubusercontent.com/mitchellkrogza/apache-ultimate-bad-bot-blocker/master/Apache_2.4/custom.d/whitelist-ips.conf -O /etc/apache2/custom.d/whitelist-ips.conf
+sudo wget https://raw.githubusercontent.com/mitchellkrogza/apache-ultimate-bad-bot-blocker/master/Apache_2.4/custom.d/whitelist-domains.conf -O /etc/apache2/custom.d/whitelist-domains.conf
+sudo wget https://raw.githubusercontent.com/mitchellkrogza/apache-ultimate-bad-bot-blocker/master/Apache_2.4/custom.d/blacklist-ips.conf -O /etc/apache2/custom.d/blacklist-ips.conf
+sudo wget https://raw.githubusercontent.com/mitchellkrogza/apache-ultimate-bad-bot-blocker/master/Apache_2.4/custom.d/bad-referrer-words.conf -O /etc/apache2/custom.d/bad-referrer-words.conf
+sudo wget https://raw.githubusercontent.com/mitchellkrogza/apache-ultimate-bad-bot-blocker/master/Apache_2.4/custom.d/blacklist-user-agents.conf -O /etc/apache2/custom.d/blacklist-user-agents.conf
+
+# **********************
+# Test the Apache Config
+# **********************
+
+printf '%s\n%s\n%s\n\n' "#################################" "Run Apache 2.4 Config Test" "#################################"
+#sudo /usr/local/apache2/bin/apachectl configtest
+
+# *********************
+# Restart Apache 2.2.25
+# *********************
+
+echo "Restarting Apache 2.2"
+#sudo /usr/local/apache2/bin/apachectl restart
+
+# ******************
+# Test Apache 2 Curl
+# ******************
+
+sudo wget -qO- http://local.dev
+
+# **********************
+# Exit With Error Number
+# **********************
+
+exit ${?}
+
+
+# MIT License
+
+# Copyright (c) 2017 Mitchell Krog - mitchellkrog@gmail.com
+# https://github.com/mitchellkrogza
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
