9e416471b0150e5d4b0876aba3be7b97b8f3fd2a
Mitchell Krog <mitchellkrog@gmail.com>
2018-08-05 15:10:19 +0200

    Revert "Trace Apache 2.2.25 Error"
    This reverts commit 777bedd34aade6c7da8f72c263bcec5de070bd49.

--- .dev-tools/install-apache-2.2.sh
+++ .dev-tools/install-apache-2.2.sh
@@ -142,9 +142,9 @@ sudo /usr/local/apache2/bin/apachectl configtest
 # Restart Apache 2.2.25
 # *********************
 
-echo "Reloading Apache 2.2"
-sudo /usr/local/apache2/bin/apachectl reload
-#sudo /usr/local/apache2/bin/apachectl start
+echo "Restarting Apache 2.2"
+sudo /usr/local/apache2/bin/apachectl stop
+sudo /usr/local/apache2/bin/apachectl start
 
 # ******************
 # Test Apache 2 Curl
@@ -152,8 +152,6 @@ sudo /usr/local/apache2/bin/apachectl reload
 
 #wget -qO- http://local.dev
 curl -sL http://local.dev
-
-echo "Done"
 
 # **********************
 # Exit With Error Number
