52b756bb37cdd1bdd2b1f58fe4cb8e79721bc1a7
Mitchell Krog <mitchellkrog@gmail.com>
2018-08-05 15:11:05 +0200

    Revert "Suppress build messages for 2.2.25"
    This reverts commit 76214b1c2b10e11a85f8fc34daf7cfaad4f8c187.

--- .dev-tools/install-apache-2.2.sh
+++ .dev-tools/install-apache-2.2.sh
@@ -59,15 +59,15 @@ wget http://www.zlib.net/zlib-1.2.11.tar.gz
 tar -xvf zlib-1.2.11.tar.gz
 cd zlib-1.2.11/
 ./configure --prefix=/usr/local
-make >/dev/null
-sudo make install >/dev/null
+make
+sudo make install
 
 wget https://github.com/mitchellkrogza/apache-ultimate-bad-bot-blocker/raw/master/.dev-tools/_apache_builds/httpd-2.2.25.tar.gz
 tar -xvf httpd-2.2.25.tar.gz
 cd httpd-2.2.25/
 ./configure --prefix=/usr/local/apache2 --enable-mods-shared=all --enable-deflate --enable-proxy --enable-proxy-balancer --enable-proxy-http
-make >/dev/null
-sudo make install >/dev/null
+make
+sudo make install
 
 sudo /usr/local/apache2/bin/apachectl start
 
