c7844f340d50618bcc441c091e6503b93f4ada07
Mitchell Krog <mitchellkrog@gmail.com>
2019-08-16 15:57:22 +0200

    Test New Spinner / Progress Bar on Apache make and install

--- .dev-tools/install-apache-2.2.sh
+++ .dev-tools/install-apache-2.2.sh
@@ -57,14 +57,68 @@ defaultcolor=$(tput setaf default)
 # FUNCTIONS
 # ---------
 
-spinner () {
-echo "THIS MAY TAKE A WHILE, PLEASE BE PATIENT ..."
-printf "["
-while kill -0 $PID 2> /dev/null; do
-    printf  "${yellow}▓"
-    sleep 1
-done
-printf "] done!"
+spinner()
+{
+  local DURATION=$1
+  local INT=0.25      # refresh interval
+
+  local TIME=0
+  local CURLEN=0
+  local SECS=0
+  local FRACTION=0
+
+  local FB=2588       # full block
+
+  trap "echo -e $(tput cnorm); trap - SIGINT; return" SIGINT
+
+  echo -ne "$(tput civis)\r$(tput el)│"                # clean line
+
+  local START=$( date +%s%N )
+
+  while [ $SECS -lt $DURATION ]; do
+    local COLS=$( tput cols )
+
+    # main bar
+    local L=$( bc -l <<< "( ( $COLS - 5 ) * $TIME  ) / ($DURATION-$INT)" | awk '{ printf "%f", $0 }' )
+    local N=$( bc -l <<< $L                                              | awk '{ printf "%d", $0 }' )
+
+    [ $FRACTION -ne 0 ] && echo -ne "$( tput cub 1 )"  # erase partial block
+
+    if [ $N -gt $CURLEN ]; then
+      for i in $( seq 1 $(( N - CURLEN )) ); do
+        echo -ne \\u$FB
+      done
+      CURLEN=$N
+    fi
+
+    # partial block adjustment
+    FRACTION=$( bc -l <<< "( $L - $N ) * 8" | awk '{ printf "%.0f", $0 }' )
+
+    if [ $FRACTION -ne 0 ]; then
+      local PB=$( printf %x $(( 0x258F - FRACTION + 1 )) )
+      echo -ne \\u$PB
+    fi
+
+    # percentage progress
+    local PROGRESS=$( bc -l <<< "( 100 * $TIME ) / ($DURATION-$INT)" | awk '{ printf "%.0f", $0 }' )
+    echo -ne "$( tput sc )"                            # save pos
+    echo -ne "\r$( tput cuf $(( COLS - 6 )) )"         # move cur
+    echo -ne "│ $PROGRESS%"
+    echo -ne "$( tput rc )"                            # restore pos
+
+    TIME=$( bc -l <<< "$TIME + $INT" | awk '{ printf "%f", $0 }' )
+    SECS=$( bc -l <<<  $TIME         | awk '{ printf "%d", $0 }' )
+
+    # take into account loop execution time
+    local END=$( date +%s%N )
+    local DELTA=$( bc -l <<< "$INT - ( $END - $START )/1000000000" \
+                   | awk '{ if ( $0 > 0 ) printf "%f", $0; else print "0" }' )
+    sleep $DELTA
+    START=$( date +%s%N )
+  done
+
+  echo $(tput cnorm)
+  trap - SIGINT
 }
 
 
@@ -96,11 +150,11 @@ cd zlib-1.2.11/
 echo "${bold}${green}Building zlib"
 printf '\n'
 make &> zlib.log &
-spinner
+spinner 30
 echo "${bold}${green}Installing zlib"
 printf '\n'
 sudo make -s install &> zlib.log &
-spinner
+spinner 30
 
 wget https://github.com/mitchellkrogza/apache-ultimate-bad-bot-blocker/raw/master/.dev-tools/_apache_builds/httpd-2.2.25.tar.gz
 tar -xvf httpd-2.2.25.tar.gz > /dev/null
@@ -109,11 +163,11 @@ cd httpd-2.2.25/
 echo "${bold}${green}Building Apache 2.2.25"
 printf '\n'
 make &> apache2build.log &
-spinner
+spinner 60
 echo "${bold}${green}Installing Apache 2.2.25"
 printf '\n'
 sudo make -s install &> apache2build.log &
-spinner
+spinner 30
 
 ${defaultcolor}
 sudo /usr/local/apache2/bin/apachectl start
